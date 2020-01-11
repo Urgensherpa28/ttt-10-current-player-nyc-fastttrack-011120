52fe2ea06dce161db0e5fd4fd4de8c5cb9782077
Urgen Sherpa <urgen.nyc@gmail.com>
2020-01-11 17:43:52 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,6 @@
 def turn_count(board)
   counter = 0 
-  if board[0, 8] == board["O"] || board[0, 8] == board["X"]
+  if board(0, 8) == board("O")|| board(0, 8) == board("X")
   counter += 1 
   end
 end
